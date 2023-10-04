a5540891054c5619f99192bd7da39bbe863e397f
Andsemandse <andr90m3@stud.kea.dk>
2023-10-04 10:12:29 +0200

    seatINIT

--- src/main/java/ways/kinobackend/repository/SeatRepository.java
+++ src/main/java/ways/kinobackend/repository/SeatRepository.java
@@ -3,6 +3,11 @@ package ways.kinobackend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ways.kinobackend.model.Seat;
 
+import java.util.Optional;
+
 public interface SeatRepository extends JpaRepository<Seat,String> {
+
+
+    Optional<Seat> findById(int id);
 }
 

7f495484e5ddcec7e85018d1166736e51f213f1e
YmerA38 <stin166c@stud.kea.dk>
2023-10-12 10:28:09 +0200

    reverse comparators fix

--- src/main/java/ways/kinobackend/coparetor/TimeslotDateComparetor.java
+++ src/main/java/ways/kinobackend/coparetor/TimeslotDateComparetor.java
@@ -10,9 +10,9 @@ public class TimeslotDateComparetor implements Comparator<Timeslot> {
     @Override
     public int compare(Timeslot timeslot1, Timeslot timeslot2) {
         if(timeslot1.getDate().isBefore(timeslot2.getDate())){
-            return 1;
-        }else if(timeslot2.getDate().isBefore(timeslot1.getDate())){
             return -1;
+        }else if(timeslot2.getDate().isBefore(timeslot1.getDate())){
+            return 1;
         }
         return 0;
     }
--- src/main/java/ways/kinobackend/coparetor/TimeslotStarttimeComparator.java
+++ src/main/java/ways/kinobackend/coparetor/TimeslotStarttimeComparator.java
@@ -8,9 +8,9 @@ public class TimeslotStarttimeComparator implements Comparator<Timeslot> {
     @Override
     public int compare(Timeslot timeslot1, Timeslot timeslot2) {
         if(timeslot1.getStart().isBefore(timeslot2.getStart())){
-            return 1;
-        }else if(timeslot2.getStart().isBefore(timeslot1.getStart())){
             return -1;
+        }else if(timeslot2.getStart().isBefore(timeslot1.getStart())){
+            return 1;
         }
         return 0;
     }
